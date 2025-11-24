c4b3d77f47ceeaeeca54be07bc069c025991c205
churley06 <conorhurley2018@outlook.com>
2025-11-24 14:25:03 +0000

    updated customer pages~

--- add_customer.php
+++ add_customer.php
@@ -1,17 +1,17 @@
-
 <?php
 include 'db_connect.php';
 
 $message = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['customer_name'];
+    $first = $_POST['first_name'];
+    $last = $_POST['last_name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $address = $_POST['address'];
 
-    $sql = "INSERT INTO customers (customer_name, email, phone, address) 
-            VALUES ('$name', '$email', '$phone', '$address')";
+    $sql = "INSERT INTO customers (first_name, last_name, email, phone, address)
+            VALUES ('$first', '$last', '$email', '$phone', '$address')";
 
     if ($conn->query($sql) === TRUE) {
         $message = "Customer added successfully!";
@@ -29,13 +29,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
 
+<nav style="background:#222; padding:12px;">
+    <a href="index.html" style="color:white; margin-right:20px;">Home</a>
+    <a href="admin_dashboard.php" style="color:white; margin-right:20px;">Admin Dashboard</a>
+</nav>
+
 <h2>Add New Customer</h2>
 
 <?php if ($message != "") echo "<p><strong>$message</strong></p>"; ?>
 
 <form action="" method="POST">
-    <label>Name:</label><br>
-    <input type="text" name="customer_name" required><br><br>
+
+    <label>First Name:</label><br>
+    <input type="text" name="first_name" required><br><br>
+
+    <label>Last Name:</label><br>
+    <input type="text" name="last_name" required><br><br>
 
     <label>Email:</label><br>
     <input type="email" name="email" required><br><br>
